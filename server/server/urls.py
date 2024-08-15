827cecf903504c364a91240f8f233e3acfd046d4
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-15 14:09:01 +0200

    style: reformated file

--- server/server/urls.py
+++ server/server/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('login', views.login)
-    re_path('signup', views.signup)
+    re_path('signup', views.signup),
+    re_path('login', views.login),
 ]
