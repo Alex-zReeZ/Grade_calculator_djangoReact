948a1561f500f5f32cc7e434a9f1df7f5b12c35d
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-15 16:43:01 +0200

    refactor: added view for users in api

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from . import views
+from .views import signup, login, allusers
 
 urlpatterns = [
-    re_path('signup', views.signup),
-    re_path('login', views.login),
+    re_path('signup', signup),
+    re_path('login', login),
+    re_path('users/', allusers),
 ]
--- server/server/views.py
+++ server/server/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(user, username=request.data['username'])
+    user = get_object_or_404(User, username=request.data['username'])
     if not user.check_password(request.data['password']):
         return Response({"detail": "Not found."}, status= status.HTTP_404_NOT_FOUND)
     token, created = Token.objects.get_or_create(user=user)
@@ -23,11 +23,15 @@ def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        user = user.objects.get(username=request.data['username'])
+        user = User.objects.get(username=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
         return Response({"token": token.key, "user": serializer.data})
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['GET'])
+def allusers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
