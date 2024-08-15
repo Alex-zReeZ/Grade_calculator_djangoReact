5104b004041b10a478110f3002027ebd567d096b
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-15 16:27:46 +0200

    chore: installed corsheader

--- server/server/settings.py
+++ server/server/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
