02bbcb513b477fc88326e0eb25cfe869b2c72e84
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-16 08:48:26 +0200

    chore: installed prettier and refomated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login.tsx';
-import Signup from './components/Signup.tsx';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login.tsx";
+import Signup from "./components/Signup.tsx";
 
 const App = () => {
   return (
