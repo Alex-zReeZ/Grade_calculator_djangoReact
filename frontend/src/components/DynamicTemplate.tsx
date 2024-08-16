0d1db2fd99c741b232a42bc7926beffc248179ac
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-16 16:02:17 +0200

    refactor: modified style on page

--- frontend/src/components/DynamicTemplate.tsx
+++ frontend/src/components/DynamicTemplate.tsx
@@ -23,26 +23,38 @@ const DynamicTemplate: React.FC<DynamicTemplateProps> = ({branchName, fetchUrl})
         fetchGrades();
     }, [fetchUrl]);
 
+    const allBranch = ["Maths", "francais", "Anglais", "Allemand", "dc-sci", "dc-maths", "dc-histoire", "eco&droit", "TIP"];
+
     return (
         <>
             <div className="p-4 bg-gray-100">
                 <header>
-                    <h1 className="w-fit px-10 p-4 bg-white rounded-2xl text-4xl font-bold text-blue-600">GradiX &nbsp; - &nbsp; {branchName}</h1>
+                    <h1 className="w-fit px-10 p-4 bg-white rounded-2xl text-4xl font-bold text-blue-600 shadow-lg">
+                        GradiX &nbsp; - &nbsp; {branchName}
+                    </h1>
                 </header>
             </div>
-            <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-                <main className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                    <section className="text-center">
-                        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-                            Notes pour {branchName}
-                        </h2>
-                        <ul>
-                            {grades.map((grade, index) => (
-                                <li key={index} className="text-gray-600 mb-2">
-                                    {grade}
-                                </li>
+            <div className="p-4 min-h-screen bg-gray-100 flex flex-col">
+                <main>
+                    <section>
+                        <div className="w-full flex space-x-[-10px]"> {/* Espace négatif entre les onglets */}
+                            {allBranch.map((branch, index) => (
+                                <a
+                                    key={index}
+                                    href={`/${branch}`}
+                                    className={`p-4 bg-white rounded-t-2xl hover:border-2 hover:border-blue-600 hover:border-b-0 hover:z-0 text-center w-[200px] font-semibold ${
+                                        branch === branchName ? "bg-gray-100 text-blue-600 border-2 border-b-0 border-blue-600 -mt-2 z-0" : ""
+                                    }`}
+                                >
+                                    {branch}
+                                </a>
                             ))}
-                        </ul>
+                        </div>
+                        <div className="p-8 px-10 bg-white w-full rounded-b-2xl">
+                            <h1>
+                                Grades
+                            </h1>
+                        </div>
                     </section>
                 </main>
             </div>
