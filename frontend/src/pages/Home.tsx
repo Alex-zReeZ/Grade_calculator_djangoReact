97744e310ba26a7e6c1d48614045cc1ade955ca0
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-22 10:36:04 +0200

    refactor: simplified code to map list of grades

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,54 @@
+// @ts-ignore
+import UserProfile from "../components/UserProfile.tsx";
+
 function Home() {
+
+  const links = [
+    {
+      href: "/French",
+      text: "Français"
+    },
+    {
+      href: "/German",
+      text: "Allemand"
+    },
+    {
+      href: "/English",
+      text: "Anglais"
+    },
+    {
+      href: "/DC-Maths",
+      text: "Maths DC"
+    },
+    {
+      href: "/Science",
+      text: "DF - Science Naturelle"
+    },
+    {
+      href: "/Maths",
+      text: "DF - Maths"
+    },
+    {
+      href: "/Histoire",
+      text: "DC - Histoire"
+    },
+    {
+      href: "/Economics and Law",
+      text: "DC - Economie et droit"
+    },
+    {
+      href: "/TIP",
+      text: "TIP",
+      className: "col-span-2"
+    }
+  ];
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <header className="text-center mb-8">
         <h1 className="text-4xl font-bold text-blue-600">GradiX</h1>
       </header>
+
 
       <main className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <section className="text-center">
@@ -16,63 +61,18 @@ function Home() {
           </p>
         </section>
         <section className="grid grid-cols-2 gap-2 w-full h-full p-5 bg-gray-100 shadow-lg rounded-2xl">
-          <a
-            href="/French"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            Français
-          </a>
-          <a
-            href="/German"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            Allemand
-          </a>
-          <a
-            href="/English"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            Anglais
-          </a>
-          <a
-            href="/DC-Maths"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            Maths DC
-          </a>
-          <a
-            href="/Science"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            DF - Science Naturelle
-          </a>
-          <a
-            href="/Maths"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            DF - Maths
-          </a>
-          <a
-            href="/Histoire"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            DC - Histoire
-          </a>
-          <a
-            href="/Economics and Law"
-            className="text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            DC - Economie et droit
-          </a>
-          <a
-            href="/TIP"
-            className="col-span-2 text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600"
-          >
-            TIP
-          </a>
+          {links.map(({href, text, className = ""}, index) => (
+              <a
+                  key={index}
+                  href={href}
+                  className={`text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600 ${className}`}
+              >
+                {text}
+              </a>
+          ))}
         </section>
       </main>
-      <footer className="mt-8 text-gray-500">© 2024 Mon App Scolaire</footer>
+      <footer className="mt-8 text-gray-500">© 2024 GradiX</footer>
     </div>
   );
 }
