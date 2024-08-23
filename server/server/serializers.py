0f2d7a308ed27c18b146712190f44812b3a71edb
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-23 13:44:39 +0200

    refactor: added serializers for grades

--- server/server/serializers.py
+++ server/server/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Grade
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
         fields = ['id', 'username', 'password', 'email']
+
+
+class GradeSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = Grade
+        fields = ['id', 'grade', 'detail']
