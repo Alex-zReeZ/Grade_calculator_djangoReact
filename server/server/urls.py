64050f8c2128a045756b9633fada009b2cac775a
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-23 15:24:38 +0200

    refactor: added view to add branches from django rest api

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,8 +1,11 @@
-from django.urls import re_path
-from .views import signup, login, allusers
+from django.urls import path
+from .views import signup, login, allusers, add_grade, get_branch_grades, add_branch
 
 urlpatterns = [
-    re_path('signup', signup),
-    re_path('login', login),
-    re_path('users/', allusers),
+    path('signup', signup),
+    path('login', login),
+    path('users/', allusers),
+    path('grades/<str:branch_name>', add_grade),
+    path('branchGrade/<str:branch_name>', get_branch_grades),
+    path('addbranch', add_branch)
 ]
--- server/server/views.py
+++ server/server/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .models import Grade, AllBranch
+from .serializers import UserSerializer, GradeSerializer, BranchSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
@@ -35,3 +36,11 @@ def allusers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def add_branch(request):
+    serializer = BranchSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
