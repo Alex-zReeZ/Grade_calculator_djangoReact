2694581d8ba7f061a7737bb48dd122734755d76d
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-29 11:10:39 +0200

    style: removed empty line

--- frontend/src/components/DisplayAverage.tsx
+++ frontend/src/components/DisplayAverage.tsx
@@ -21,7 +21,6 @@ const DisplayAverage: React.FC<DisplayAverageProps> = ({ grades, module }) => {
       <h2 className="text-xl font-semibold mt-6">Average:</h2>
       <div className="flex items-center space-x-4 mt-2">
           <GradeColor grade={roundedAverage} />
-
       </div>
     </div>
   );
