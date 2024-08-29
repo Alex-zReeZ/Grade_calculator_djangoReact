a19460b3b3ea95fe71a7154a2ff459f2f2ed3bd2
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-29 11:40:47 +0200

    refactor: average display a no grade yet if there is no grade

--- frontend/src/components/GradeColor.tsx
+++ frontend/src/components/GradeColor.tsx
@@ -1,16 +1,22 @@
 export function GradeColor({ grade }) {
   let color;
-  if (grade > 4.5) {
+  if (grade === 0) {
+    color = "#000000";
+  } else if (grade > 4.5) {
     color = "#32de84";
   } else if (grade < 4) {
     color = "#AA0000";
+  } else if (grade < 1) {
+    color = "#000000";
   } else {
     color = "#FEBE10";
   }
+
   return (
+    grade === 0 ? "No Grade Yet" :
     <span
       className="w-fit text-lg font-semibold p-5 py-3 border-2 bg-white rounded-2xl"
-      style={{ color: color, borderColor: color }}
+      style={{color: color, borderColor: color}}
     >
       {grade}
     </span>
