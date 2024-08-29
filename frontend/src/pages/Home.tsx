052c2f3e1f6d0ab7d3f05630a43e3aae0143690d
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-29 08:46:14 +0200

    style: reformated file

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -48,41 +48,41 @@ function Home() {
   ];
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <header className="text-center mb-8">
-        <h1 className="text-4xl font-bold text-blue-600">GradiX</h1>
-      </header>
-
-      <UserProfile />
-
-      <main className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <section className="text-center">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-            Bienvenue sur le Calculateur de Notes
-          </h2>
-          <p className="text-gray-600 mb-6">
-            Utilisez cet outil pour calculer vos notes et obtenir la moyenne de
-            vos résultats scolaires.
-          </p>
-        </section>
-        <section className="grid grid-cols-2 gap-2 w-full h-full p-5 bg-gray-100 shadow-lg rounded-2xl">
-          {links.map(({ href, text, className = "" }, index) => (
-            <a
-              key={index}
-              href={href}
-              className={`text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600 ${className}`}
-            >
-              {text}
-            </a>
-          ))}
-        </section>
-        <div className="text-center text-gray-500 mt-7">
-          <p>*DF: Discipline Fondamentale</p>
-          <p>*DC: Discipline Complémentaire</p>
-        </div>
-      </main>
-      <footer className="mt-8 text-gray-500">© 2024 GradiX</footer>
-    </div>
+    <>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <header className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-blue-600">GradiX</h1>
+        </header>
+        <UserProfile />
+        <main className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+          <section className="text-center">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+              Bienvenue sur le Calculateur de Notes
+            </h2>
+            <p className="text-gray-600 mb-6">
+              Utilisez cet outil pour calculer vos notes et obtenir la moyenne
+              de vos résultats scolaires.
+            </p>
+          </section>
+          <section className="grid grid-cols-2 gap-2 w-full h-full p-5 bg-gray-100 shadow-lg rounded-2xl">
+            {links.map(({ href, text, className = "" }, index) => (
+              <a
+                key={index}
+                href={href}
+                className={`text-center p-4 bg-white shadow-md rounded-lg hover:border-2 hover:border-blue-600 ${className}`}
+              >
+                {text}
+              </a>
+            ))}
+          </section>
+          <div className="text-center text-gray-500 mt-7">
+            <p>*DF: Discipline Fondamentale</p>
+            <p>*DC: Discipline Complémentaire</p>
+          </div>
+        </main>
+        <footer className="mt-8 text-gray-500">© 2024 GradiX</footer>
+      </div>
+    </>
   );
 }
 
