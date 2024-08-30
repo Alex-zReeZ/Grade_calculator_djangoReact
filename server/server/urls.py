d042c152e65e73a9e082db3d7c1f54e4ae4e30a6
Alex-zReeZ <alexbeck220@gmail.com>
2024-08-30 10:28:14 +0200

    refactor: replace all path with django viewsets and router

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from .views import signup, login, allusers, add_grade, add_branch, get_grades_by_branch
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import UserViewSet, GradeViewSet, BranchViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'grades', GradeViewSet, basename='grade')
+router.register(r'branches', BranchViewSet)
 
 urlpatterns = [
-    path('signup', signup),
-    path('login', login),
-    path('users/', allusers),
-    path('addbranch', add_branch),
-    path('grades/<str:branch_name>', add_grade),
-    path('grades/<str:branch_name>/list', get_grades_by_branch),
+    path('', include(router.urls)),
 ]
--- server/server/views.py
+++ server/server/views.py
@@ -1,70 +1,66 @@
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 from .models import Grade, AllBranch, BranchGrade
 from .serializers import UserSerializer, GradeSerializer, BranchSerializer
-from rest_framework import status
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
 
-from django.shortcuts import get_object_or_404
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-
-@api_view(['POST'])
-def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response({"detail": "Not found."}, status= status.HTTP_404_NOT_FOUND)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    return Response({"token": token.key, "user": serializer.data})
-
-@api_view(['POST'])
-def signup(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        user = User.objects.get(username=request.data['username'])
-        user.set_password(request.data['password'])
-        user.save()
-        token = Token.objects.create(user=user)
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        user = get_object_or_404(User, username=request.data['username'])
+        if not user.check_password(request.data['password']):
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(instance=user)
         return Response({"token": token.key, "user": serializer.data})
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def allusers(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+    @action(detail=False, methods=['post'])
+    def signup(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            user = User.objects.get(username=request.data['username'])
+            user.set_password(request.data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return Response({"token": token.key, "user": serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
-def add_grade(request, branch_name):
-    all_branch = get_object_or_404(AllBranch, name=branch_name)
-    branch_grade, created = BranchGrade.objects.get_or_create(branch=all_branch)
-    serializer = GradeSerializer(data=request.data)
+class GradeViewSet(viewsets.ModelViewSet):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
 
-    if serializer.is_valid():
-        grade = serializer.save()
-        grade.branch = branch_grade
-        grade.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @action(detail=True, methods=['post'])
+    def add_grade(self, request, pk=None):
+        all_branch = get_object_or_404(AllBranch, name=pk)
+        branch_grade, created = BranchGrade.objects.get_or_create(branch=all_branch)
+        serializer = GradeSerializer(data=request.data)
 
-@api_view(['POST'])
-def add_branch(request):
-    serializer = BranchSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            grade = serializer.save()
+            grade.branch = branch_grade
+            grade.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def get_grades_by_branch(request, branch_name):
-    try:
-        branch = AllBranch.objects.get(name=branch_name)
-    except AllBranch.DoesNotExist:
-        return Response({'error': 'Branch not found'}, status=status.HTTP_404_NOT_FOUND)
+    @action(detail=True, methods=['get'])
+    def get_grades_by_branch(self, request, pk=None):
+        try:
+            branch = AllBranch.objects.get(name=pk)
+        except AllBranch.DoesNotExist:
+            return Response({'error': 'Branch not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    grades = Grade.objects.filter(branch__branch=branch)
-    serializer = GradeSerializer(grades, many=True)
-    return Response(serializer.data)
+        grades = Grade.objects.filter(branch__branch=branch)
+        serializer = GradeSerializer(grades, many=True)
+        return Response(serializer.data)
+
+class BranchViewSet(viewsets.ModelViewSet):
+    queryset = AllBranch.objects.all()
+    serializer_class = BranchSerializer
