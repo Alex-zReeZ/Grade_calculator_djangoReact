793f43ca47387f74343669d9a0b0ffe6255c8bec
Alex-zReeZ <alexbeck220@gmail.com>
2024-09-04 15:54:45 +0200

    refactor: added error popup when a login fail

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
     try {
       const response = await fetch("http://localhost:8000/users/login/", {
         method: "POST",
@@ -22,10 +24,12 @@ const Login = () => {
         document.cookie = `token=${data.token}; path=/; max-age=${60 * 60 * 24}`;
         console.log("Login successful, token stored in cookies");
       } else {
+        setError("Une erreur s'est produite. Veuillez vérifier les informations entrées.");
         console.error("Login failed, no token received");
       }
       console.log(data);
     } catch (error) {
+      setError("Une erreur s'est produite. Veuillez vérifier les informations entrées.");
       console.error(error);
     }
   };
@@ -36,6 +40,33 @@ const Login = () => {
         <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
           Login
         </h1>
+        {error && (
+          <div className="rounded-md bg-red-200 p-4 lg:mr-0 lg:max-w-lg mb-5">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#f87171"
+                  aria-hidden="true"
+                  className="h-full pr-10"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <div className="ml-3">
+                <p className="text-sm font-medium text-red-500">
+                  Une erreur s'est produite. Veuillez vérifier les informations
+                  entrées.
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
