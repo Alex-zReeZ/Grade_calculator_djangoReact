fa35a2bea51ff1e6bd412b1befc4105380e5231d
Alex-zReeZ <alexbeck220@gmail.com>
2024-09-04 13:53:56 +0200

    refactor: added variable that store a branch id

--- frontend/src/components/DisplayAverage.tsx
+++ frontend/src/components/DisplayAverage.tsx
@@ -1,12 +1,43 @@
 // @ts-ignore
 import { GradeColor } from "./GradeColor.tsx";
+import { useState, useEffect } from "react";
 
 interface DisplayAverageProps {
   grades: { grade: number }[];
   module: boolean;
+  branchId: number | null;
 }
 
-const DisplayAverage: React.FC<DisplayAverageProps> = ({ grades, module }) => {
+const DisplayAverage: React.FC<DisplayAverageProps> = ({ grades, module, branchId }) => {
+
+  const [newAverage, setNewAverage] = useState<number>();
+
+  useEffect(() => {
+    const HandleAverage = async (branchId: number) => {
+      try {
+        const response = await fetch(`http://localhost:8000/branches/${branchId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setNewAverage(data.average);
+      } catch (error) {
+        console.error("Error fetching average grade:", error);
+        return null;
+      }
+    };
+
+    if (branchId !== null) {
+      HandleAverage(branchId);
+    }
+  }, [branchId]);
+
   const average =
     grades.length > 0
       ? grades.reduce((acc, grade) => acc + grade.grade, 0) / grades.length
@@ -20,7 +51,7 @@ const DisplayAverage: React.FC<DisplayAverageProps> = ({ grades, module }) => {
     <div className="">
       <h2 className="text-xl font-semibold mt-6">Average:</h2>
       <div className="flex items-center space-x-4 mt-2">
-        <GradeColor grade={roundedAverage} />
+        <GradeColor grade={newAverage} />
       </div>
     </div>
   );
